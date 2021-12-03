850f0efdcf968841b89c6c48dc7535b540aadf81
william <wyq123445@outlook.com>
2021-12-03 11:04:14 +0800

    完成了reactor客户端的编写

--- src/main/java/Reactor/EchoClient/Connector.java
+++ src/main/java/Reactor/EchoClient/Connector.java
@@ -1,4 +1,29 @@
 package Reactor.EchoClient;
 
-public class Connector {
+import java.io.IOException;
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+
+public class Connector implements Runnable {
+    private final Selector selector;
+
+    private final SocketChannel socketChannel;
+
+    public Connector(SocketChannel socketChannel, Selector selector) {
+        this.socketChannel = socketChannel;
+        this.selector = selector;
+    }
+
+    @Override
+    public void run() {
+        try {
+            if (socketChannel.finishConnect()) {
+                System.out.println(String.format("已完成 %s 的连接",
+                        socketChannel.getRemoteAddress()));
+                new Handler(socketChannel, selector);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/Reactor/EchoClient/EchoClient.java
+++ src/main/java/Reactor/EchoClient/EchoClient.java
@@ -2,6 +2,6 @@ package Reactor.EchoClient;
 
 public class EchoClient {
     public static void main(String[] args) {
-
+        new Thread(new NIOClient("127.0.0.1", 2333)).start();
     }
 }
